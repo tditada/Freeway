6f88d5d7b2efd25fedecc370b870cb8b6b02269e
Teresa di Tada <tditada@gmail.com>
2015-08-22 20:07:33 -0300

    More car's rows, score added

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text scoreText;
+	public int score;
+	public GameController current;
+
+	void Start () {
+		current = this;
+		score = 0;
+		UpdateScore ();
+	}
+
+	public void AddScore(int newScore){
+		score += newScore;
+		UpdateScore ();
+	}
+
+	void UpdateScore () {
+		scoreText.text = "Score: " + score;
+	}
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,7 +10,27 @@ public class PlayerScript : MonoBehaviour {
 
 	public float speed = 100;
 	public float step = 0.07f;
+	public float offset = 0.01f;
+	public int addScore = 1;
 	public YBoundary yBoundary;
+	GameController gameController;
+
+	void Start(){
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject) {
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (!gameController) {
+			Debug.Log ("Cannot Find GameController Instance");
+		}
+	}
+
+	void Update(){
+		if (transform.position.y >= yBoundary.yMax-offset) {
+			gameController.current.AddScore(addScore);
+			MoveToStart ();
+		}
+	}
 
 	void FixedUpdate () {
 
@@ -27,6 +47,11 @@ public class PlayerScript : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(){
+		MoveToStart ();
+		//gameController.AddScore ((-1) * addScore);
+	}
+
+	void MoveToStart(){
 		this.transform.position = new Vector2 (0,yBoundary.yMin);
 	}
 	
