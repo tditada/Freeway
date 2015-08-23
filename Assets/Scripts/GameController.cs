87ebb71bcc20e83691bcb70bc0d7a9e8d901cf79
Teresa di Tada <tditada@gmail.com>
2015-08-23 03:27:07 -0300

    Main Menu added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,13 +8,35 @@ public class GameController : MonoBehaviour {
 	public Text scoreTextPlayerTwo;
 	public int scorePlayerOne;
 	public int scorePlayerTwo;
+	public int maxTimeInSec;
+	public int game;
 	public GameController current;
+
 
 	void Start () {
 		current = this;
+		game = 1;
 		scorePlayerOne = 0;
 		scorePlayerTwo = 0;
+		maxTimeInSec = 10;
 		UpdateScore ();
+	}
+
+	void Update(){
+		if (Time.timeSinceLevelLoad > maxTimeInSec) {
+			if(scorePlayerOne > scorePlayerTwo){
+				scoreTextPlayerOne.text = "Winner";
+				scoreTextPlayerTwo.text = ":(";
+			}else if(scorePlayerTwo > scorePlayerOne){
+				scoreTextPlayerOne.text = ":(";
+				scoreTextPlayerTwo.text = "Winner";
+			}else{
+				scoreTextPlayerOne.text = "DRAW";
+				scoreTextPlayerTwo.text = "DRAW";
+			}
+			//Application.LoadLevel("MainScene");
+
+		}
 	}
 
 	public void AddScore(int newScore, int player){
---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void OnClick(int level){
+		Application.LoadLevel (level);
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
